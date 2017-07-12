bf85d5afd1194cc71f1c41254b4fe9e2994d8394
zjxiaobai <897665728@qq.com>
2017-07-12 15:43:55 +0800

    no

--- 工程实践大作业阿里云服务器版/web端/jkxystudent/src/com/jkxy/conn/conn.java
+++ 工程实践大作业阿里云服务器版/web端/jkxystudent/src/com/jkxy/conn/conn.java
@@ -9,8 +9,8 @@ public class conn {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			String url="jdbc:mysql://localhost/jkxystudent?useUnicode=true&characterEncoding=utf-8";
-			String user="root";
-			String password="583483asd";
+			String user="test";
+			String password="123456";
 			Connection conn=DriverManager.getConnection(url, user,password);
 			System.out.println(conn.getMetaData().getURL());
 			return conn;
